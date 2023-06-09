3d4cb80793b77113c8b6ec14a75bdc8d99f8714c
Ahmed Maher <ahmededu015@gmail.com>
2023-06-09 21:17:18 +0300

    Add game validators.

---
+++ validators/gameQuestionValidator.js
@@ -0,0 +1,36 @@
+const joi = require("joi");
+
+exports.createGameQuestion = joi.object({
+  content: joi.string().min(5).max(200).required(),
+  choices: joi
+    .object({
+      A: joi.string().required(),
+      B: joi.string().required(),
+      C: joi.string().required(),
+      D: joi.string().required(),
+    })
+    .required(),
+  answer: joi.string().valid("A", "B", "C", "D").required(),
+  subjectId: joi.number().integer().required(),
+  period: joi
+    .number()
+    .min(5 * 60)
+    .max(2 * 60 * 60)
+    .required(), // min 5 mins and max 2 hours
+});
+
+exports.updateGameQuestion = joi.object({
+  content: joi.string().min(5).max(200),
+  choices: joi.object({
+    A: joi.string(),
+    B: joi.string(),
+    C: joi.string(),
+    D: joi.string(),
+  }),
+  answer: joi.string().valid("A", "B", "C", "D"),
+  subjectId: joi.number().integer(),
+  period: joi
+    .number()
+    .min(5 * 60)
+    .max(2 * 60 * 60), // min 5 mins and max 2 hours
+});
--- validators/gameValidator.js
+++ validators/gameValidator.js
@@ -1,25 +1,15 @@
 const joi = require("joi");
 const moment = require("moment");
 
-exports.createExam = joi.object({
+exports.createGame = joi.object({
   name: joi.string().min(5).max(100).required(),
-  period: joi
-    .number()
-    .min(5 * 60)
-    .max(2 * 60 * 60)
-    .required(), // min 5 mins and max 2 hours
-
   departmentId: joi.number().integer().required(),
   photo: joi.string(),
   startedAt: joi.date().min(moment(Date.now())).required(),
 });
 
-exports.updateExam = joi.object({
+exports.updateGame = joi.object({
   name: joi.string().min(5).max(100),
-  period: joi
-    .number()
-    .min(5 * 60)
-    .max(2 * 60 * 60), // min 5 mins and max 2 hours
   departmentId: joi.number().integer(),
   photo: joi.string(),
   startedAt: joi.date().min(moment(Date.now())),
