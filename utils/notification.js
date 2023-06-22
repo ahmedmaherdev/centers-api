30474dcf0fd7c948cb8fad61d94ed991cc9add34
Ahmed Maher <ahmededu015@gmail.com>
2023-06-23 00:02:13 +0300

    Translate the notification to ar

--- utils/notification.js
+++ utils/notification.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
-
 const serviceAccount = require("../config/serviceAccount.json");
+const translate = require("./translate");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -15,15 +15,21 @@ class Notification {
   }
 
   async send() {
-    const message = {
-      notification: {
-        title: this.title,
-        body: this.body,
-      },
-      data: { data: JSON.stringify(this.data) },
-      tokens: this.deviceTokens,
-    };
-    return await admin.messaging().sendMulticast(message);
+    try {
+      const title = await translate(this.title);
+      const body = await translate(this.body);
+      const message = {
+        notification: {
+          title,
+          body,
+        },
+        data: { data: JSON.stringify(this.data) },
+        tokens: this.deviceTokens,
+      };
+      return await admin.messaging().sendMulticast(message);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
